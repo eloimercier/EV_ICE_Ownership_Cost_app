3f341eff0ec7a2896e4f1350c17c6d20bf4782ca
eloimercier <39504654+eloimercier@users.noreply.github.com>
2023-08-09 19:42:59 -0700

    Added dashboard and read data

--- app/server.R
+++ app/server.R
@@ -1,3 +1,22 @@
 
 server <- function(input, output, session) {
+
+##############################################################
+######################### READ XLSX ##########################
+##############################################################
+
+data <- reactiveValues(car_data=NA)
+
+observe({
+	a <- read.xlsx("../data/EV_list_Canada.xlsx", sheet="Cars")
+	TOTO <<- a
+ 	data$car_data <- a
+})
+
+output$car_table <- renderDT(
+	data$car_data
+	)
+
+
+
 }
--- app/ui.R
+++ app/ui.R
@@ -1,4 +1,39 @@
 
-ui <- fluidPage(
-  "Hello, world!"
+ui <- dashboardPage(
+
+##############################################################
+############################ HEADER ##########################
+##############################################################
+
+  dashboardHeader(title = "EV List and Comparator"),
+
+##############################################################
+########################### SIDEBAR ##########################
+##############################################################
+
+  dashboardSidebar(
+    sidebarMenu(
+        menuItem("Setup", tabName = "setup", icon = icon("circle-info")),
+        menuItem("Car List", tabName = "carlist", icon = icon("list")),
+        menuItem("Compare", tabName = "compare", icon = icon("chart-line")),
+        menuItem("Parameters", tabName = "params", icon = icon("database"))
+    )
+  ), #end dashboardSidebar
+
+##############################################################
+############################ BODY ############################
+##############################################################
+
+  dashboardBody(
+
+############################
+#CAR LIST
+############################
+  tabItem(tabName = "carlist",
+    DTOutput("car_table")
+  )
+
+
+  ) #end dashboardBody
+
 )
