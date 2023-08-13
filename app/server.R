b9fcd519f31285f9637fcc7228d46d38f418a092
eloimercier <39504654+eloimercier@users.noreply.github.com>
2023-08-12 21:21:46 -0700

    Updated region selection

--- app/server.R
+++ app/server.R
@@ -5,16 +5,16 @@ server <- function(input, output, session) {
 ######################### READ DATA ##########################
 ##############################################################
 
-	data <- reactiveValues(car_data=NA, rebates=NA, taxes=NA, gas=NA, electricity=NA, fees=NA, sources=NA)
+	dataTables <- reactiveValues(car_data=NA, rebates=NA, taxes=NA, gas=NA, electricity=NA, fees=NA)
+	dataVariable <- reactiveValues(region=NA, federal_rebate=NA, federal_max_msrp=NA,  region_rebate=NA, region_max_msrp=NA, tax=NA, gas_rate=NA, electricity_rate=NA)
 
 	observe({
-		data$car_data <- read.xlsx("../data/EV_list_Canada.xlsx", sheet="Cars")
-		data$rebates <- read.xlsx("../data/EV_list_Canada.xlsx", sheet="Rebates")
-		data$taxes <- read.xlsx("../data/EV_list_Canada.xlsx", sheet="Taxes")
-		data$gas <- read.xlsx("../data/EV_list_Canada.xlsx", sheet="Gas")
-		data$electricity <- read.xlsx("../data/EV_list_Canada.xlsx", sheet="Electricity")
-		data$fees <- read.xlsx("../data/EV_list_Canada.xlsx", sheet="Fees")
-		data$sources <- read.xlsx("../data/EV_list_Canada.xlsx", sheet="Sources")
+		dataTables$car_data <- read.xlsx("../data/EV_list_Canada.xlsx", sheet="Cars", check.names = TRUE)
+		dataTables$rebates <- read.xlsx("../data/EV_list_Canada.xlsx", sheet="Rebates")
+		dataTables$taxes <- read.xlsx("../data/EV_list_Canada.xlsx", sheet="Taxes", rowNames = TRUE)
+		dataTables$gas <- read.xlsx("../data/EV_list_Canada.xlsx", sheet="Gas", rowNames = TRUE)
+		dataTables$electricity <- read.xlsx("../data/EV_list_Canada.xlsx", sheet="Electricity", rowNames = TRUE)
+		dataTables$delivery_fees <- read.xlsx("../data/EV_list_Canada.xlsx", sheet="Fees", rowNames = TRUE)
 	})
 
 ##############################################################
@@ -22,47 +22,71 @@ server <- function(input, output, session) {
 ##############################################################
 
 output$setupUI <- renderUI({
-	province_list <- unique(data$gas[,1])
+	region_list <- unique(dataTables$rebates[,1])
+	region_list <- region_list[-c(which(region_list=="Federal"), grep("Source", region_list))]
         tagList(
-          	selectInput('province', 'Province/Territory:', c(Choose='', province_list), selectize=FALSE),
-          	conditionalPanel(condition = "input.province == 'British Columbia'",
-  				selectInput("income", "Income:", list("Less than 80k$ ind. income", "Between 80k$ and 90k$ ind. income", "More than 90k$ ind. income"))
-			)
+          	selectInput('region', 'Province/Territory:', c(Choose='', region_list), selectize=FALSE),
+          	conditionalPanel(condition = "input.region == 'British Columbia'",
+    				    selectInput("income", "Income:", list("Less than 80k$ ind. income", "Between 80k$ and 90k$ ind. income", "More than 90k$ ind. income"))
+  			    )
         )
 })
+	
+observe({
+  if(!is.null(input$region) & !identical(input$region,'')){
+      
+    dataVariable$region <- input$region
 
-output$rebate_table <- renderDT({
-	rebates <- data$rebates
-	if(!is.null(input$province) & !identical(input$province,'')){
-		if(input$province=='British Columbia'){
-			rebates[rebates[,1] == input$province & rebates[,4]==input$income,,drop=FALSE]
-		} else {
-			rebates[rebates[,1] == input$province,,drop=FALSE]
-		}
-	}
+    #Calculate rebates
+    rebates <- dataTables$rebates
+    region_occurences <- table(dataTables$rebates[,"Region"])
+    region_with_multiple_rebate <- names(region_occurences)[region_occurences>1] #which regions has variable rebates?
+    
+    dataVariable$federal_rebate <- as.numeric(rebates[rebates[,1] == "Federal","Maximum.amount"])
+    dataVariable$federal_max_msrp <- as.numeric(rebates[rebates[,1] == "Federal","If.MSRP.below..."])
+    
+    if(input$region %in% region_with_multiple_rebate){
+      dataVariable$region_rebate <-  as.numeric(rebates[rebates[,1] == input$region & rebates[,4]==input$income,"Maximum.amount",drop=TRUE])
+      dataVariable$region_max_msrp <-  as.numeric(rebates[rebates[,1] == input$region & rebates[,4]==input$income,"If.MSRP.below...",drop=TRUE])
+    } else {
+      dataVariable$region_rebate <-  as.numeric(rebates[rebates[,1] == input$region,"Maximum.amount",drop=TRUE])
+      dataVariable$region_max_msrp <-  as.numeric(rebates[rebates[,1] == input$region,"If.MSRP.below...",drop=TRUE])
+    }
+
+    #Set rates
+    dataVariable$tax <- as.numeric(dataTables$taxes["Federal",1]) + as.numeric(dataTables$taxes[input$region,1])
+    dataVariable$gas_rate  <- as.numeric(dataTables$gas[input$region,1])
+    dataVariable$electricity_rate <- as.numeric(dataTables$electricity[input$region,1])
+  }
+  
 })
 
+
 output$rebate_info <- renderUI({
-	rebates <- data$rebates
-	REBATES <<- rebates
-	if(!is.null(input$province) & !identical(input$province,'')){
-		federal_rebate <- rebates[rebates[,1]=="Federal","Maximum.amount"]
-
-		if(input$province=='British Columbia'){
-			province_rebate <- rebates[rebates[,1] == input$province & rebates[,4]==input$income,"Maximum.amount",drop=FALSE]
-		} else {
-			province_rebate <- rebates[rebates[,1] == input$province,"Maximum.amount",drop=FALSE]
-		}
-
-
-		HTML(paste0('<b>','Federal Rebate:</b> $',federal_rebate,'<br>',
-					'<b>','Provincial Rebate:</b> $',province_rebate,'<br>'
-			))
-
+	rebates <- dataTables$rebates
+	if(!is.null(input$region) & !identical(input$region,'')){
+		federal_rebate <- dataVariable$federal_rebate
+		federal_msrp <- dataVariable$federal_max_msrp
+		region_rebate <- dataVariable$region_rebate
+		region_msrp <- dataVariable$region_max_msrp
+		
+		federal_rebate_text <- ifelse(federal_rebate>0,
+		                              paste0("Up to ",federal_rebate,"$"),
+		                              "No rebate.")
+		region_rebate_text <- ifelse(region_rebate>0,
+		                             paste0("Up to ",region_rebate,"$"),
+		                             "No rebate.")
+		federal_msrp_text <- ifelse(!is.na(federal_msrp),
+                          		  paste0(" on vehicles below $",federal_msrp,"."),
+                          		  "")
+		region_msrp_text <- ifelse(!is.na(region_msrp),
+	                              paste0(" on vehicles below $",region_msrp,"."),
+	                              "")
+		
+		HTML(paste0('<b>','Federal Rebate: </b>',federal_rebate_text, federal_msrp_text,'<br>',
+					'<b>','Provincial Rebate: </b>',region_rebate_text, region_msrp_text,'<br>'
+		))
 	}
-
-
-
 })
 
 ##############################################################
@@ -70,9 +94,16 @@ output$rebate_info <- renderUI({
 ##############################################################
 
 
-output$car_table <- renderDT(
-	data$car_data
-)
+output$car_table <- renderDT({
+	car_list <- dataTables$car_data
+	car_list <- car_list[,-which(colnames(car_list)=="Link")]
+	delivery_fees <- sapply(car_list$Maker, function(x){dataTables$delivery_fees[x,1]})
+	delivery_fees[is.na(delivery_fees)] <- 0
+	car_list$after_tax_fees <- (car_list[,"MSRP..CAD."] + delivery_fees) * (1+dataTables$taxes["Federal tax",]+dataTables$taxes[input$region,1]) #(msrp + delivery) * taxes
+	car_list$after_rebates <- car_list$after_tax_fees - ifelse(car_list$Engine,
+	                                                           car_list$rebates ,0)
+	car_list
+}, selection = 'single')
 
 
 
