9bfe7a994798df2b0a312bc2039e8662bd923af9
eloimercier <39504654+eloimercier@users.noreply.github.com>
2024-02-24 16:25:05 -0800

    Changed UI

--- app/ui.R
+++ app/ui.R
@@ -1,150 +1,125 @@
 
-ui <- dashboardPage(skin = "green",
+ui <- fluidPage(
 
-##############################################################
-############################ HEADER ##########################
-##############################################################
-
-    dashboardHeader(title = "EV List and Comparator", titleWidth = 300,
-                    tags$li(class = "dropdown",tags$a(
-                            actionButton("info", "App Info",  style="color: black; background-color: transparent; border-color: transparent; margin:0px 0px 0px 0px; padding:0px 0px 0px 0px; font-size:100%"),  style=" margin:0px 0px 0px 0px; padding:13px 10px 10px 10px; font-size:100%"
-                        )),
-                    tags$li(class = "dropdown", tags$a(
-                        img(src="https://img.icons8.com/?size=512&id=62856&format=png",  width="30", height="30"), href = "https://github.com/eloimercier/EV_app", style="margin:0px 0px 0px 0px; padding:10px 10px 10px 10px;"
-                        ))
-),
-
-##############################################################
-########################### SIDEBAR ##########################
-##############################################################
-
-    dashboardSidebar(
       tags$style("@import url(https://use.fontawesome.com/releases/v5.7.2/css/all.css);"), #to load all fonts
-        sidebarMenu(id="sidebarID",
-            menuItem("User Info", tabName = "user_info", icon = icon("user")),
-            menuItem("Car List", tabName = "carlist", icon = icon("list")),
-            menuItem("Compare", tabName = "compare", icon = icon("chart-line")),
-            menuItem("Parameters", tabName = "params", icon = icon("database"))
-        )
-    ), #end dashboardSidebar
+
+##############################################################
+############################ LEFT PANEL ##########################
+##############################################################
+
+    titlePanel("User Info"),
+
+
+    sidebarLayout(
+    
+##############################################################
+############################ LEFT PANEL ##########################
+##############################################################
+
+    sidebarPanel(
+        actionButton("walkthroughBtn", label="Interactive Walkthrough", icon.library="font awesome",css.class='sc-button'),
+        uiOutput("user_countryUI"),
+        uiOutput("user_regionUI"),
+        uiOutput("user_region_specificUI"),
+        uiOutput("user_kms_yearsUI"),
+        div(style = "height:20px"),
+        uiOutput("rebate_info")
+    ),
 
 ##############################################################
 ############################ BODY ############################
 ##############################################################
 
-    dashboardBody(
       
-     useConductor(), #for dynamic walkthrough
-      
-        tabItems(
-############################
-#USER INFO
-############################
-        tabItem(tabName = "user_info",
-            actionButton("walkthroughBtn", label="Interactive Walkthrough", icon.library="font awesome",css.class='sc-button'),
-            uiOutput("user_countryUI"),
-            uiOutput("user_regionUI"),
-            uiOutput("user_region_specificUI"),
-            uiOutput("user_kms_yearsUI"),
-            div(style = "height:20px"),
-            uiOutput("rebate_info")
-        ),
+        mainPanel(
+            tabsetPanel(
 
-############################
-#CAR LIST
-############################
+                ############################
+                #USER INFO
+                ############################        
+                # tabPanel("user_info", 
+                #     plotOutput("plot")), 
 
-        tabItem(tabName = "carlist",
-           dataTableOutput("car_table")
-        ),
+                ############################
+                #CAR LIST
+                ############################
+                tabPanel("Car Overview",value="car_list_tab", 
+                    h3("Car prices are given for reference only and might not reflect current pricing."),
+                    dataTableOutput("car_table")
+                ), 
 
-############################
-#COMPARISON
-############################
-
-tabItem(tabName = "compare",
-        fluidRow(
-        #Car selection UI
-            column(12, 
-                fluidRow(
-                    column(2,uiOutput("CarSelection0UI")), # rownames
-                    column(2,uiOutput("CarMake1UI"), uiOutput("CarModel1UI"), uiOutput("CarTrim1UI")), 
-                    column(2,uiOutput("CarMake2UI"), uiOutput("CarModel2UI"), uiOutput("CarTrim2UI")), 
-                    column(2,uiOutput("CarMake3UI"), uiOutput("CarModel3UI"), uiOutput("CarTrim3UI")), 
-                    column(2,uiOutput("CarMake4UI"), uiOutput("CarModel4UI"), uiOutput("CarTrim4UI")), 
-                    column(2,uiOutput("CarMake5UI"), uiOutput("CarModel5UI"), uiOutput("CarTrim5UI")) 
-                )
-            ),
-
-        #Model variables
-        column(12, 
-            div(style = "height:20px"),
-            
-            HTML("<b>Parameters used to estimate cost of ownership</b>"),
-            bsCollapse(id = "model_variable_table_collapsible", open = NULL,
-                bsCollapsePanel("Model Variables  <click here to edit>", value="model_variable_collapse",
-                    uiOutput("model_variable_info"),
-                    dataTableOutput("CarSelectedVariableTable"), 
-                    style="info")
-            ),
-            div(style = "height:20px")
-        ),
-
-        #Comparison table and plot
-        column(12,
-            HTML('<b>Cost of ownership based on model variables</b>'),
-
-            tabsetPanel(id="comparison_panels",
-                tabPanel("Table", value="table",
-
+                ############################
+                #COMPARISON
+                ############################
+                tabPanel("Car Comparator",value="comparison_tab", 
                     fluidRow(
+                        #Car selection UI
+                        column(12, 
+                            fluidRow(
+                                column(2,uiOutput("CarSelection0UI")), # rownames
+                                column(2,uiOutput("CarMake1UI"), uiOutput("CarModel1UI"), uiOutput("CarTrim1UI")), 
+                                column(2,uiOutput("CarMake2UI"), uiOutput("CarModel2UI"), uiOutput("CarTrim2UI")), 
+                                column(2,uiOutput("CarMake3UI"), uiOutput("CarModel3UI"), uiOutput("CarTrim3UI")), 
+                                column(2,uiOutput("CarMake4UI"), uiOutput("CarModel4UI"), uiOutput("CarTrim4UI")), 
+                                column(2,uiOutput("CarMake5UI"), uiOutput("CarModel5UI"), uiOutput("CarTrim5UI")) 
+                            )
+                        ),
 
-                        #pruchase price
-                        dataTableOutput("purchasePriceTable"),
-                        #cost of ownsership over the years
-                        dataTableOutput("CarComparisonTable"),
-                        #final cost of ownership
-                        dataTableOutput("CostOfOwnershipTable"),
+                        #Model variables
+                        column(12, 
+                            div(style = "height:20px"),
+                            
+                            HTML("<b>Parameters used to estimate cost of ownership</b>"),
+                            bsCollapse(id = "model_variable_table_collapsible", open = NULL,
+                                bsCollapsePanel("Model Variables  <click here to edit>", value="model_variable_collapse",
+                                    uiOutput("model_variable_info"),
+                                    dataTableOutput("CarSelectedVariableTable"), 
+                                    style="info")
+                            ),
+                            div(style = "height:20px")
+                        ),
 
-                        #Final cost after resale
-                        div(style = "height:30px"),
-                        column(12, uiOutput("car_resell_info")),
-                        dataTableOutput("BreakdownCostTable"),
-                        dataTableOutput("CarFinalCostTable")
+                        #Comparison table and plot
+                        column(12,
+                            HTML('<b>Cost of ownership based on model variables</b>'),
+
+                            tabsetPanel(id="comparison_panels",
+                                tabPanel("Table", value="table",
+
+                                    fluidRow(
+
+                                        #pruchase price
+                                        dataTableOutput("purchasePriceTable"),
+                                        #cost of ownsership over the years
+                                        dataTableOutput("CarComparisonTable"),
+                                        #final cost of ownership
+                                        dataTableOutput("CostOfOwnershipTable"),
+
+                                        #Final cost after resale
+                                        div(style = "height:30px"),
+                                        column(12, uiOutput("car_resell_info")),
+                                        dataTableOutput("BreakdownCostTable"),
+                                        dataTableOutput("CarFinalCostTable")
+                                    )
+                                ),
+                                tabPanel(title="Plot", value="plot",
+                                    plotlyOutput("combinedCarPlot")
+                                )      
+                            )
+                        )
                     )
+
                 ),
-                tabPanel(title="Plot", value="plot",
-                    plotlyOutput("combinedCarPlot")
-                )      
-            )
-        )
 
-    )
-
-      
-
-        
-),
-
-
-############################
-#PARAMETERS
-############################
-
-        tabItem(tabName = "params",
-                tabsetPanel(id="param_panels",
-                  tabPanel(title="Rebates",  DTOutput("rebate_table", width="800px"), uiOutput("rebate_source")),
-                  tabPanel(title="Taxes",  DTOutput("tax_table", width="600px"), uiOutput("tax_source")),
-                  tabPanel(title="Gas",  DTOutput("gas_table", width="600px"), uiOutput("gas_source")),
-                  tabPanel(title="Electricity",  DTOutput("electricity_table", width="600px"), uiOutput("electricity_source")),
-                  tabPanel(title="Delivery Fees",  DTOutput("delivery_fees_table", width="600px")),
-                  tabPanel(title="Default Model Variables", value="default_model_variable", DTOutput("default_model_variable_table", width="600px"))
+                ############################
+                #PARAMETERS
+                ############################
+                tabPanel("Default Parameters", value="params_tab", 
+                    tabPanel(title="Rebates",  DTOutput("rebate_table", width="800px"), uiOutput("rebate_source"))
                 )
-        )
 
 
-############################
-        ) #end tabItems
-    ) #end dashboardBody
-
-)
+            ) #end tabsetPanel
+        ) #end mainPanel
+    ) #end sidebarLayout
+) #end fluidPage
